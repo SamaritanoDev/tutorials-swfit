1d48244120d1652d8cc3f0bd24cc8e249e101310
Lesly Samaritano Ayala <1628112@utp.edu.pe>
2024-03-11 14:11:45 -0500

    feat: structure vide detail

--- listsGrid/listsGrid/ContentView.swift
+++ listsGrid/listsGrid/ContentView.swift
@@ -11,12 +11,54 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             List(list){item in
-                Text(item.name)
-            }.navigationTitle("Mi Lista")
+               NavigationLink(
+                destination: ViewDetail(items: item)
+               ){
+                    HStack {
+                        emoji(emoji: item)
+                        Text(item.name)
+                            .font(.subheadline)
+                    }
+                }
+            }.navigationTitle("Mi emojis fav")
         }
     }
 }
 
-#Preview {
-    ContentView()
+struct emoji: View {
+    let emoji: Model
+    
+    var body: some View{
+        ZStack{
+            Text(emoji.emoji)
+                .shadow(color: .pink,radius: 3)
+                .font(.largeTitle)
+                .frame(width: 65, height: 65)
+                .overlay(Circle()
+                    .stroke(Color.pink, lineWidth: 3))
+        }
+    }
+}
+
+struct ViewDetail: View{
+    var items: Model
+    
+    var body: some View{
+        VStack(alignment: .leading, spacing: 3, content: {
+            HStack{
+                emoji(emoji: items)
+                Text(items.name)
+                    .font(.title)
+                    .bold()
+                Spacer()
+            }
+                Text(items.description)
+                    .padding(.top)
+                Spacer()
+                
+            
+        })
+        .padding(.all)
+        .navigationTitle("Emoji")
+    }
 }
--- listsGrid/listsGrid/Model.swift
+++ listsGrid/listsGrid/Model.swift
@@ -16,23 +16,28 @@ struct Model: Identifiable {
 
 let list: [Model] = [
     Model(
-           emoji: "🙋🏼‍♂️",
-           name: "Mano alzada",
-           description: "Persona que levanta la mano para preguntar."),
+           emoji: "👩🏽‍💻",
+           name: "Mujer desarrolladora",
+           description: "Persona que crea aplicaciones web, móvil, etc."),
+    
+    Model(
+           emoji: "🎙️",
+           name: "Micrófono",
+           description: "Accesorio para usarlo en Podcast, etc."),
+    Model(
+           emoji: "💻",
+           name: "Laptop",
+           description: "Computadora portátil para usarlo en cualquier lugar y espacio."),
+    Model(
+           emoji: "✨",
+           name: "Estrellas",
+           description: "Destacar algo y que brilla en el Cielo."),
+    Model(
+           emoji: "💜",
+           name: "Corazón morado",
+           description: "Representa el apoyo hacia otra persona."),
     Model(
            emoji: "🥑",
            name: "Aguacate",
            description: "El aguacate es un fruto exótico carnoso que se obtiene del árbol tropical del mismo name. En algunas partes de América del Sur se conoce como Palta.."),
-    Model(
-           emoji: "🍕",
-           name: "Pizza",
-           description: "Comida chatarra creada en Italia."),
-    Model(
-           emoji: "🧩",
-           name: "Pieza de rompecabezas",
-           description: "Los rompecabezas o puzzles son piezas comúnmente planas que combinadas correctamente forman una figura, un objeto o una escena. ... Por diversión, entretenimiento o como una forma de relajarse"),
-    Model(
-           emoji: "🗽",
-           name: "Estatua de la Libertad",
-           description: "Estatua ubicada en Nuva York."),
 ]
